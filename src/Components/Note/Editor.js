2785004dc016acd9f9c1cd76c24ed67eeb41f4ee
djamel <djadweb@gmail.com>
2021-11-15 17:52:16 +0100

    CKEditor integration

--- src/Components/Note/Editor.js
+++ src/Components/Note/Editor.js
@@ -1,53 +1,30 @@
 import React from 'react';
-import EditorJS from '@editorjs/editorjs'; 
-import Header from '@editorjs/header'; 
-import List from '@editorjs/list';
-import Embed from '@editorjs/embed';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 function Editor() {
-    const editor = new EditorJS({          
-        holder: 'editor-js',             
-        tools: {
-            header: {
-                class: Header,
-                inlineToolbar: ['link']
-            },
-            list: {
-                class: List,
-                inlineToolbar: [
-                    'link',
-                    'bold'
-                ]
-            },
-            embed: {
-                class: Embed,
-                inlineToolbar: false,
-                config: {
-                    services: {
-                        youtube: true,
-                        coub: true
-                    }
-                }
-            }
-        },
-        autofocus: true,
-        placeholder: 'Write here...'
-    });    
+    
 
-    // editor.isReady.then(() => {
-    //     editor.save().then((data) => {
-    //         console.log(data);
-    //     }).catch((error) => {
-    //         console.log(error);
-    //     })
-    // })
-
-    return (
-        
+    return (        
         <div className="note__editor" id="editor-js">
-        
+            <CKEditor
+                    editor={ ClassicEditor }                    
+                    onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
         </div>
-
     )
 }
 
