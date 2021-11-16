3e567efc9dbb3f5cbe86078b2b2b7a2b1ef3a3f6
djamel <djadweb@gmail.com>
2021-11-16 18:57:03 +0100

    edit note title & save note with title in localstorage

--- src/Components/Note/Editor.js
+++ src/Components/Note/Editor.js
@@ -2,27 +2,16 @@ import React from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-function Editor() {
-    
-
+function Editor({getNote}) {
     return (        
         <div className="note__editor" id="editor-js">
             <CKEditor
-                    editor={ ClassicEditor }                    
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
+                    editor={ ClassicEditor }                                        
                     onChange={ ( event, editor ) => {
                         const data = editor.getData();
-                        console.log( { event, editor, data } );
+                        getNote(data);
                     } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
+                   
                 />
         </div>
     )
--- src/Components/Note/Note.js
+++ src/Components/Note/Note.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Editor from './Editor';
 import EditIcon from '@mui/icons-material/Edit';
 import './Note.scss';
 
-function Note() {            
+function Note() {    
+    const [title, setTitle] = useState("");
+    const [value, setValue] = useState("");
+
+    const handleTitle = (e) => {        
+        setTitle(e.target.value);
+    }
+
+    const getNote = (value) => {
+        setValue(value);
+    }
+
+    const saveNote = () => {
+        const note = {
+            title : title,
+            value : value
+        }
+        console.log(note)
+        let notes;
+        console.log(notes)
+        if(localStorage.getItem("notes") === null) {
+            notes = [];
+            console.log(notes)
+        } else {
+            notes = JSON.parse(localStorage.getItem("notes"));        
+            console.log(notes)
+        }
+        console.log(notes)
+        
+        notes.push(note);console.log(notes)
+        localStorage.setItem("notes", JSON.stringify(notes));
+        console.log(JSON.parse(localStorage.getItem("notes")));            
+    }
     return (
         <div className="note">            
             <div className="note__header">
                 <div className="edit-icon">
                     <EditIcon/>
                 </div>
-                <div className="note-title">New note</div>
+                <button onClick={() => saveNote()} className="saveBtn">Save</button>
+                {/* <button onClick={(e) => handleTitle(e)} className="note-title">{title}</button> */}
+                <input className="note-title" onChange={(e) => handleTitle(e)} placeholder="New Note" value={title}/>
             </div>            
-            <Editor/>
+            <Editor getNote={getNote}/>
         </div>
         
     )
